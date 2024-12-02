7b298a6a71df376799e7931ce59c8565502dfcb6
Sarah Cristina <sarahsilva34567@gmail.com>
2024-12-02 20:24:40 -0300

    Cria Animmal.css e .tsc. Edita arquivos e cria as classes de Animal

--- src/Animal.tsx
+++ src/Animal.tsx
@@ -1,5 +1,24 @@
 import "./Animal.css";
 
-export default function Animal(){
-    return <div className="animal">Animal</div>;
+interface AnimalProps{
+    icone?: string;
+    nome?: string;
+    peso?: number;
+    emExtincao?: boolean;
+}
+
+export default function Animal({
+    icone = "?",
+    nome =  "desconhecido",
+    peso = 0.0,
+    emExtincao = false,
+}:AnimalProps){
+    return(
+        <div className="animal">
+        <div className="icone">{icone}</div>
+        <div className="nome">{nome}</div>
+        <div className="peso">{peso.toLocaleString()}Kg</div>
+        {emExtincao && <div className="extincao">Em Extinção!</div>}
+        </div>
+    );
 }
--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,14 @@ import './App.css'
 function App() {
  
 
-  return <div className = "app"><Animal/>
-    </div>;
+  return (
+  <div className = "app">
+        <Animal icone= "🐔" nome="Cocó" peso={1.0} emExtincao={true}/>
+        <Animal icone= "🦩" nome="Flamingo" peso={12.0} emExtincao={true}/>
+        <Animal icone="🐧" nome="Penguin" peso={6.0} emExtincao={true}/>
+        <Animal icone= "🦜" nome="Paraguaio" peso={2.0} emExtincao={false}/>
+    </div>
+  );
 }
    
 
