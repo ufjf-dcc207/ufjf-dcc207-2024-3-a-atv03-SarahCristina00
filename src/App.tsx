af924869dc21371f0bacfb901834d6e01122bc81
Sarah Cristina <sarahsilva34567@gmail.com>
2024-12-05 22:16:47 -0300

    atualiza css, conclui exibição,  e cria children

--- src/App.tsx
+++ src/App.tsx
@@ -6,22 +6,17 @@ function App() {
   return (
     <div className="app">
       <Exibicao
-        inicio={new Date("2024-12-02T08:00:00.000-03:00")}
-        fim={new Date("2024-12-02T12:00-03:00")}
-        local="A1"
-      >
+        abertura={new Date("2024-12-02T08:00:00.000-03:00")}
+        fechamento={new Date("2024-12-02T12:00-03:00")} cercado="A1"/>
+
         <Animal icone="🐔" nome="Cocó" peso={1.0} emExtincao={true} />
         <Animal icone="🦩" nome="Flamingo" peso={12.0} emExtincao={true} />
-      </Exibicao>
-      
       <Exibicao
-        inicio={new Date("2024-12-02T14:00:00.000-03:00")}
-        fim={new Date("2024-12-02T16:00-03:00")}
-        local="B2"
-      >
+        abertura={new Date("2024-12-02T14:00:00.000-03:00")}
+        fechamento={new Date("2024-12-02T16:00-03:00")}
+        cercado="B2"/>
         <Animal icone="🐧" nome="Penguin" peso={6.0} emExtincao={true} />
         <Animal icone="🦜" nome="Paraguaio" peso={2.0} emExtincao={false} />
-      </Exibicao>
     </div>
   );
 }
--- src/Exibicao.tsx
+++ src/Exibicao.tsx
@@ -1,22 +1,27 @@
+import {ReactNode} from "react";
 import './Exibicao.css';
 
+
 interface ExibicaoProps{
-    inicio: Date;
-    fim: Date;
-    local: string;
+    abertura: Date;
+    fechamento: Date;
+    cercado: string;
+    children?: ReactNode;
 }
 
 export default function Exibicao({
-    inicio,
-    fim,
-    local: cercado,
+    abertura:inicio,
+    fechamento:fim,
+    cercado: local,
+    children,
 
 }:ExibicaoProps){
     return(
         <div className="exibição">
         <div className="inicio">Abertura: {inicio.toLocaleString()}</div>
         <div className="fim">Encerramento: {fim.toLocaleString()}</div>
-        <div className="cercado">Local: {cercado.toLocaleString()}</div>
+        <div className="local">Local: {local.toLocaleString()}</div>
+        <div className="animais">{children}</div>
         </div>
     );
 }
